dd21fed7e700aaf1797e9b43fe373083a88d79c6
Brad House <brad@brad-house.com>
2025-10-06 11:22:36 -0400

    add ceph rules

---
+++ network_hypervisor.tf
@@ -0,0 +1,89 @@
+locals {
+  subnet_hypervisor  = "10.10.100.0/24"
+  hypervisors        = [ "10.10.100.2", "10.10.100.3", "10.10.100.4" ]
+
+  aclrules_access_ceph = {
+    start_idx = 40000
+    rules     = [
+      {
+        description  = "access ceph mon v1"
+        action       = "allow"
+        cidr_list    = [ local.hypervisors ]
+        protocol     = "tcp"
+        port         = "6789"
+        traffic_type = "egress"
+      },
+      {
+        description  = "access ceph mon v2"
+        action       = "allow"
+        cidr_list    = [ local.hypervisors ]
+        protocol     = "tcp"
+        port         = "3300"
+        traffic_type = "egress"
+      },
+      {
+        description  = "access ceph osd and mds"
+        action       = "allow"
+        cidr_list    = [ local.hypervisors ]
+        protocol     = "tcp"
+        port         = "6800-7568"
+        traffic_type = "egress"
+      }
+    ]
+  }
+
+  aclrules_hypervisor = {
+    start_idx = 30000
+    rules     = [
+      {
+        description  = "ceph mon v1"
+        action       = "allow"
+        cidr_list    = [ "0.0.0.0/0" ]
+        protocol     = "tcp"
+        port         = "6789"
+        traffic_type = "ingress"
+      },
+      {
+        description  = "ceph mon v2"
+        action       = "allow"
+        cidr_list    = [ "0.0.0.0/0" ]
+        protocol     = "tcp"
+        port         = "3300"
+        traffic_type = "ingress"
+      },
+      {
+        description  = "ceph osd and mds"
+        action       = "allow"
+        cidr_list    = [ "0.0.0.0/0" ]
+        protocol     = "tcp"
+        port         = "6800-7568"
+        traffic_type = "ingress"
+      }
+    ]
+  }
+
+  aclrules_hypervisor_all = concat(local.aclrules_common, [ local.aclrules_hypervisor ])
+}
+
+resource "cloudstack_network_acl" "hypervisor" {
+  name   = "hypervisor"
+  vpc_id = cloudstack_vpc.infra_vpc.id
+}
+
+module "network_acl_hypervisor" {
+  source    = "./modules/cloudstack_network_acl"
+  acl_id    = cloudstack_network_acl.hypervisor.id
+  managed   = true
+  bootstrap = var.bootstrap
+  rulelist  = local.aclrules_hypervisor_all
+}
+
+resource "cloudstack_private_gateway" "default" {
+  gateway             = "10.10.100.99"
+  ip_address          = "10.10.100.99"
+  netmask             = "255.255.255.0"
+  vlan                = "vlan://"  # Cloudstack has this marked as required
+  acl_id              = cloudstack_network_acl.hypervisor.id
+  vpc_id              = cloudstack_vpc.infra_vpc.id
+  physical_network_id = "mgmt"
+}
